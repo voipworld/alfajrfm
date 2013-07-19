80b679cf3b958bc92ed9b65cbfdac7ab5dcc1aa0
Tayeb Meftah <tayeb@vobradio.org>
2013-07-19 13:34:26 +0200

    Revert "update to the background fix"
    This reverts commit a700cba291fdfe61e68194f6f3631740b693d56b.

--- iOS/Alfajr FM/HomeVC.h
+++ iOS/Alfajr FM/HomeVC.h
@@ -7,10 +7,33 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "RadioPlayer.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface HomeVC : UIViewController <RadioPlayerDelegate>{
+#import <arpa/inet.h> // For AF_INET, etc.
+#import <ifaddrs.h> // For getifaddrs()
+#import <net/if.h> // For IFF_LOOPBACK
+
+
+
+#import "Reachability.h"
+#import <SystemConfiguration/SystemConfiguration.h>
+
+#import "URLS.h"
+
+@class AVPlayer;
+@class AVPlayerItem;
+
+
+@interface HomeVC : UIViewController {
     
+    AVAsset *asset;
+    AVPlayerItem *playerItem;
+    AVPlayer *player;
+    NSURL *mURL;
+    
+    
+	IBOutlet MPVolumeView *airplay;
     IBOutlet UILabel *nowplaying;
     IBOutlet UILabel *metadatas;
     IBOutlet UIToolbar *toolBar;
@@ -18,44 +41,66 @@
     IBOutlet UIBarButtonItem *pauseButton;
     IBOutlet UIBarButtonItem *playButton2;
     IBOutlet UIBarButtonItem *stopButton;
-    IBOutlet UIBarButtonItem *shareButton;
+     IBOutlet UIBarButtonItem *shareButton;
     IBOutlet UIBarButtonItem *flexible;
-    IBOutlet UILabel *alaqsaUILabel;
-    IBOutlet UINavigationBar *navBar2;
+   // IBOutlet UI
     
-    RadioPlayer *radioPlayer;
+    //prefs
+    
+    BOOL use3G;
+    BOOL streamingHighQuality;
+    BOOL alwaysUseLowQualityWhenOn3G;
+    
+    
+    IBOutlet UILabel *alaqsa;
+  
+    
+    Reachability* internetReachable;
+    Reachability* hostReachable;
+ 
+    UIAlertView *alert;
+    
+    URLS *streamDefinitions;
+    NSString *stream1;
+    NSString *stream2;
+    
+    IBOutlet UINavigationBar *navBar2;
 }
+@property(nonatomic,retain)IBOutlet UINavigationBar *navBar2;
 
 
 
+@property (nonatomic) BOOL *allowsAirPlay;
+@property(nonatomic,retain) IBOutlet MPVolumeView *airplay;
+
+@property (nonatomic, retain) IBOutlet UILabel *nowplaying;
+@property (nonatomic, retain) IBOutlet UILabel *metadatas;
 @property (retain) IBOutlet UIToolbar *toolBar;
+
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *playButton;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *pauseButton;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *playButton2;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *stopButton;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *flexible;
 @property (nonatomic,retain)  IBOutlet UIBarButtonItem *shareButton;
-@property (strong, nonatomic) IBOutlet UILabel *alaqsaUILabel;
-@property (nonatomic, retain) IBOutlet UILabel *nowplaying;
-@property (nonatomic, retain) IBOutlet UILabel *metadatas;
-@property(nonatomic,retain)IBOutlet UINavigationBar *navBar2;
+
+@property (nonatomic, assign) CMTime movieDuration;
+
+@property (strong, nonatomic) IBOutlet UILabel *alaqsa;
+
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath   ofObject:(id)object
+                        change:(NSDictionary *)change context:(void *)context;
+- (BOOL)isPlaying;
+- (void)enablePlayerButtons;
+- (void)disablePlayerButtons;
+
 
 - (IBAction)play:(id)sender;
 - (IBAction)pause:(id)sender;
 - (IBAction)stop:(id)sender;
 
-
-/* Radio player delegate methods -- these get called by RadioPlayer instance */ 
-
--(void)radioDidPause;
--(void)radioDidPlay;
--(void)radioDidStop;
-
--(void)setMetadata:(NSString *)metadata;
--(BOOL)getNowPlayingHiddenBool;
--(void)setNowPlayingHiddenBool:(BOOL)boolean;
-
-
-
+-(void)reachabilityDidChange:(NSNotification *) notif;
     
 @end
